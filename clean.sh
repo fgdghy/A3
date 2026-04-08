b3bd5621f95fb73055eb85f6222d8beef25239f2
Linyu <lilinyu12345@gmail.com>
2026-04-08 18:50:21 -0400

    add

---
+++ clean.sh
@@ -0,0 +1,14 @@
+export DB_HOST="bookstore-db-dev-instance1.c2qxlb8d3arf.us-east-1.rds.amazonaws.com"
+
+mysql -h $DB_HOST -u admin -p'12345678' -e "
+    USE customer_db; 
+    SET FOREIGN_KEY_CHECKS = 0; 
+    TRUNCATE TABLE customers; 
+    SET FOREIGN_KEY_CHECKS = 1;
+
+    USE book_db; 
+    SET FOREIGN_KEY_CHECKS = 0; 
+    TRUNCATE TABLE related_books; 
+    TRUNCATE TABLE books; 
+    SET FOREIGN_KEY_CHECKS = 1;
+"
